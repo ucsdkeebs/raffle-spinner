702d9d8b09bdca2fad96f607cd72f1dd6e62d14e
cwang04 <117037369+cwang04@users.noreply.github.com>
2023-12-01 20:49:11 -0800

    comments

--- public/spin.js
+++ public/spin.js
@@ -1,6 +1,6 @@
 let names = [];
 let slots = [];
-const numSlots = 7;
+const numSlots = 8;
 const minRoll = 40;
 
 //adds delay to functions
@@ -8,65 +8,55 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// function to spin the wheel
 async function rollNames() {
-    var start = Math.floor(Math.random() * names.length); //what the index of the start is
-    var spins = Math.floor(Math.random() * names.length) + minRoll; // the number of times to spin the wheel      
-
-    // to implement a slot machine wheel, the lowest value has to be the first index, and we need a buffer of length//2 to get the value in the middle
-    // need to think about storing a variable in order to reset the placement of the 
-    await gsap.set(".slot", {
-        top: "50vh", // Move elements down by random variable so that arrow appears to be smoother
-    });
-
-    //include a spin animation and then do this manual spin so that it looks like wheel is slowing down
+    var start = Math.floor(Math.random() * names.length); // picks a random index to start the spin
+    var spins = Math.floor(Math.random() * names.length) + minRoll; // the number of times to spin the wheel, with built in spin so that it always looks like it spins  
+    
+    // iterates for the number of spins
     for (let i = start; i <= start + spins; i++) {
         let delay = 0.05;
+        // checks if there is less than 30 spins left, then starts to slow down the spin
         if (i >= start + spins - 30) {
-            delay = (0.05 + (0.02 * (i - (start + spins - 30)) / 5));
+            delay = (0.05 + (0.02 * (i - (start + spins - 30)) / 5)); //every 5 spins, slow down by 20 ms
         }
-        // roll down starting from the top
-        await gsap.to(".slot", {
+        // 
+        await gsap.to(".slot", { // animates a slide downward
             duration: delay, // Animation duration in seconds
             y: "+=10vh", // Move each element down by one slot
             ease: "power4.out", // Easing function 
-            // after roll completed, resets the divs with new values, i.e. slot2 goes back with the value of the old slot3 so the roll is complete
+            // after roll completed, resets the divs with new values, i.e. slot2 goes back to its original place, but with the value of the old slot3 so the roll is complete
             onComplete: () => {
                 for (let j = numSlots; j > 0; j--) {
-                    var dynamicTextElement = document.getElementById("slot" + (numSlots + 1 - j));
+                    var dynamicTextElement = document.getElementById("slot" + (numSlots - j)); // finds each of the slots
                     if (dynamicTextElement) {
-                        // subtract half of slots to get it into the center of the slots
-                        dynamicTextElement.textContent = names[(i + j - 4) % names.length] ;
+                        // sets the content of the slot to new value, subtracts half of the numSlots, so that the winning value is in the middle
+                        dynamicTextElement.textContent = names[(i + j - (numSlots / 2)) % names.length] ;
                     }
                 }
-                gsap.set(".slot", {
+                gsap.set(".slot", { //set resets the slots to their original place
                     y: "-=10vh"
                 })
             }
         });        
     }
 
-    // move this to be outside the for loop -1 so that it looks smoother transition
-    /* looks extremely bad rn, should be implemented inside of the main loop, so that it looks more complete, need to alter clip paths as well for this to look right.
-    await gsap.to(".slot", {
-        duration: 1, // Animation duration in seconds
-        y: "+="+(Math.random() * 6)+"vh", // Move elements down by random variable so that arrow appears to be smoother
-        ease: "power4.out", // Easing function
-    });*/
-    await sleep(50);
+    await sleep(50); // delay to make the animation smoother
     
-    alert (names[(start + spins) % names.length] + " has won!");
+    let winIndex = (start + spins) % names.length; //gets the winning index to be displayed/removed
+    alert (names[winIndex] + " has won!");
     
     // remove winner
-    names.splice((start + spins) % names.length, 1);
+    names.splice(winIndex, 1);
 }
 
-// generate
+// default start
 async function startState() {
     // fills array on start
     names = Array.from(Array(100).keys(), n => n + 1);
 
-    for (let i = 1; i <= numSlots; i++) {
-        var dynamicTextElement = document.getElementById("slot" + i)
+    for (let i = 0; i < numSlots; i++) {
+        var dynamicTextElement = document.getElementById("slot" + i);
         if (dynamicTextElement) {
             var dynamicText = i;
             dynamicTextElement.textContent = dynamicText;
