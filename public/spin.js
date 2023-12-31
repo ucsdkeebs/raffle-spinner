3282b1051295fb8ab82e5f99722b6599eb994e1d
cwang04 <cow004@ucsd.edu>
2023-12-31 15:11:18 -0500

    Fixed multiple button click and increased spin time

--- public/spin.js
+++ public/spin.js
@@ -1,6 +1,6 @@
 let names = [];
 const numSlots = 10;
-const minRoll = 73; //minimum to have the roll speed be 5.5 seconds
+const minRoll = 73; //minimum to have the roll speed be 6.93 seconds
 
 //adds delay to functions
 function sleep(ms) {
@@ -9,6 +9,10 @@ function sleep(ms) {
 
 // function to spin the wheel
 async function rollNames() {
+    //disable button
+    var button = document.getElementById("roll");
+    button.onclick = null;
+
     closeModal();
     var start = Math.floor(Math.random() * names.length); // picks a random index to start the spin
     var spins = Math.floor(Math.random() * 20) + minRoll; // the number of times to spin the wheel, with built in spin so that it always looks like it spins 
@@ -16,9 +20,9 @@ async function rollNames() {
     // iterates for the number of spins
     for (let i = start; i <= start + spins; i++) {
         let delay = 0.05;
-        // checks if there is less than 30 spins left, then starts to slow down the spin
-        if (i >= start + spins - 30) {
-            delay = (0.05 + (0.02 * (i - (start + spins - 30)) / 5)); //slows down the spin by 0.004 seconds.
+        // checks if there is less than 40 spins left, then starts to slow down the spin
+        if (i >= start + spins - 40) {
+            delay = (0.05 + (0.02 * (i - (start + spins - 40)) / 5)); //slows down the spin by 0.004 seconds.
         }
         // 
         await gsap.to(".slot", { // animates a slide downward
@@ -38,7 +42,7 @@ async function rollNames() {
                     y: "-=8vh"
                 })
             }
-        });        
+        });   
     }
 
     await sleep(50); // delay to make the animation smoother
@@ -61,17 +65,25 @@ async function rollNames() {
         names = new Array();
         closeModal();
     };
+
+    // resets the functionality of the button
+    button.onclick = function() {
+        rollNames();
+    };
 }
 
 // default start
 async function startState() {
     // fills array on start
-    names = Array.from(Array(100).keys(), n => n + 1);
+    names = Array.from(Array(1000).keys(), n => n + 1);
+    // shuffles the numbers in an array
+    shuffle(names);
 
     for (let i = 0; i < numSlots; i++) {
         var dynamicTextElement = document.getElementById("slot" + i);
         if (dynamicTextElement) {
-            var dynamicText = i;
+            // randomizes where to put the names on the wheel
+            var dynamicText = names[Math.floor(Math.random()*names.length)];
             dynamicTextElement.textContent = dynamicText;
         }
         console.log(i);
@@ -79,11 +91,31 @@ async function startState() {
     }
 }
 
+// to shuffle our array on start
+function shuffle(array) {
+    let currentIndex = array.length
+    let randomIndex = 0;
+  
+    // while elements left to shuffle
+    while (currentIndex > 0) {
+  
+      // pick random element and swap with current element
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
+// closes pop up window
 async function closeModal() {
     var modal = document.getElementById("winScreen");
     modal.style.display = "none";
 }
 
+// opens pop up window with winner and options
 function openModal(winningNumber) {
     var modal = document.getElementById("winScreen");
     var dynamicTextElement = document.getElementById("modal-text");
